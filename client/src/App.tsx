9520fbbc63cad0ed9ead2aa5fdfa40dfcc208305
unknown <skrepkov-vova01@mail.ru>
2022-10-21 19:06:29 +0300

    edit

---
+++ client/src/App.tsx
@@ -0,0 +1,22 @@
+import React, { useEffect } from 'react';
+
+function App() {
+  
+  useEffect(() => {
+    fetch("http://localhost:8080/hi", {
+      method: "GET"
+    }).then(res => res.json()).then(res => {
+      
+      console.log(res);
+      debugger;
+    })
+  }, [])
+  
+  return (
+    <div className="App">
+      Test
+    </div>
+  );
+}
+
+export default App;
--- server/main.go
+++ server/main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"net/http"
-	"os"
-
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"net/http"
+	"os"
 )
 
 func main() {
@@ -14,9 +13,17 @@ func main() {
 
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+	}))
 
 	e.GET("/", func(c echo.Context) error {
-		return c.HTML(http.StatusOK, "Hello, Docker! <3")
+		return c.HTML(http.StatusOK, "Hello, dsdasDocker! <3")
+	})
+
+	e.GET("/hi", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hi")
 	})
 
 	e.GET("/ping", func(c echo.Context) error {
